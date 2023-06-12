8c243bded0cbcf03ac7e4f106bf90f87ba3d757c
Marcus Freire <marcusfreiremartins@gmail.com>
2023-06-11 23:54:02 -0300

    Adicionar arquivos de projeto.

---
+++ Recursive/Branch.cs
@@ -0,0 +1,29 @@
+﻿namespace Recursive
+{
+    public class Branch
+    {
+        List<Branch> branches = new List<Branch>();
+
+        public void AddBranch(Branch branch)
+        {
+            branches.Add(branch);
+        }
+
+        public int CalculateDepth()
+        {
+            if (branches.Count == 0)
+            {
+                return 1;
+            }
+
+            int maxDepth = 0;
+            foreach (var branch in branches)
+            {
+                int depth = branch.CalculateDepth();
+                maxDepth = Math.Max(maxDepth, depth);
+            }
+
+            return maxDepth + 1;
+        }
+    }
+}
---
+++ Recursive/Program.cs
@@ -0,0 +1,26 @@
+﻿using Recursive;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Branch rootBranch = new Branch();
+            Branch branch1 = new Branch();
+            Branch branch2 = new Branch();
+            Branch branch3 = new Branch();
+            Branch branch4 = new Branch();
+            Branch branch5 = new Branch();
+            Branch branch6 = new Branch();
+
+            rootBranch.AddBranch(branch1);
+            rootBranch.AddBranch(branch2);
+            branch1.AddBranch(branch3);
+            branch3.AddBranch(branch4);
+            branch4.AddBranch(branch5);
+            branch5.AddBranch(branch6);
+
+            int depth = rootBranch.CalculateDepth();
+
+            Console.WriteLine("Depth of the structure: " + depth);
+        }
+    }
